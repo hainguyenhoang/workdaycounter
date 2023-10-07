9617a536e2e50a61c115290999e5ff768f21d045
Hai Nguyen Hoang <hai.nguyenhoang@proton.me>
2023-10-07 10:24:29 +0200

    final 0.1.0 version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use chrono::prelude::*;
 use std::{fs, thread};
 use std::collections::HashSet;
 use std::str::FromStr;
+use chrono::prelude::*;
 use chrono::Duration;
 use clap::Parser;
 use console::Term;
@@ -17,60 +17,56 @@ struct Args {
     #[arg(long)]
     now: Option<NaiveDateTime>,
 
-    /// The last day at work in %Y-%m-%d format e.g.: 2023-11-17
-    last_workday: NaiveDate,
-    /// The time when you end work in %H:%M format, e.g: 16:00. An 8 hour shift is assumed.
-    end_of_worktime: NaiveTime
+    /// The date and time when you end work in %Y-%m-%dT%H:%M:%S%.f format, e.g.: 2023-11-17T16:00:00.0. An 8 hour shift is assumed.
+    end_of_work: NaiveDateTime
 }
 
-fn parse_holidays(args: &Args) -> HashSet<NaiveDate> {
+fn parse_holidays(holiday_opt: &Option<String>) -> HashSet<NaiveDate> {
     let mut holidays = HashSet::new();
-    if let Some(holiday_path) = &args.holiday_path {
-        let holiday_file_string = fs::read_to_string(holiday_path).expect("Unable to read holiday file");
-        for holiday_str in holiday_file_string.split('\n') {
-            if let Ok(holiday) = NaiveDate::from_str(holiday_str) {
-                holidays.insert(holiday);
+    if let Some(holiday_path) = holiday_opt {
+        if let Ok(holiday_file_string) = fs::read_to_string(holiday_path){
+            for holiday_str in holiday_file_string.split('\n') {
+                if let Ok(holiday) = NaiveDate::from_str(holiday_str) {
+                    holidays.insert(holiday);
+                }
             }
         }
     }
     holidays
 }
 
-fn calculate_diff_for_today(now: &NaiveDateTime, args: &Args) -> f64{
-    if now.weekday().number_from_monday() <= 5 {
-        let seconds_remaining_today = (args.end_of_worktime - now.time()).num_seconds();
+fn calculate_remaining_workdays(now: &NaiveDateTime, end_of_work: &NaiveDateTime,
+                                      holidays: &HashSet<NaiveDate>) -> f64{
+    let mut ret = 0.0;
+    if now.weekday().number_from_monday() <= 5 && !holidays.contains(&now.date()){
+        let seconds_remaining_today = (end_of_work.time() - now.time()).num_seconds();
         let today_remaining = seconds_remaining_today as f64 / (8 * 60 * 60) as f64;
-        today_remaining.max(0.0)
+        ret += today_remaining.min(1.0).max(0.0);
     }
-    else {
-        0.0
+    let mut date_to_check = now.date() + Duration::days(1);
+    while date_to_check <= end_of_work.date() {
+        if date_to_check.weekday().number_from_monday() <= 5 && !holidays.contains(&date_to_check){
+            ret += 1.0;
+        }
+        date_to_check += Duration::days(1);
     }
+    ret
 }
 
 fn main() {
     let args = Args::parse();
-    let holidays = parse_holidays(&args);
+    let holidays = parse_holidays(&args.holiday_path);
 
     let term = Term::stdout();
     loop {
-        let mut diff = 0.0;
         let mut now = Local::now().naive_local();
         if let Some(new_now) = args.now {
             now = new_now;
         }
 
-        diff += calculate_diff_for_today(&now, &args);
-
-        let mut date_to_check = now.date() + Duration::days(1);
-        while date_to_check <= args.last_workday {
-            if date_to_check.weekday().number_from_monday() <= 5 && !holidays.contains(&date_to_check){
-                diff += 1.0;
-            }
-            date_to_check += Duration::days(1);
-        }
-
         term.clear_last_lines(1).unwrap();
-        println!("Workdays remaining: {}", diff);
+        println!("Workdays remaining: {}", calculate_remaining_workdays(&now, &args.end_of_work,
+                                                                        &holidays));
         if let Some(_) = args.now {
             break;
         }
